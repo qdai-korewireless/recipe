11ace0400874b98289e9688630d313c3a4b15c9d
Quan Dai <qdai@koresystemsgroup.com>
2017-12-06 11:47:58 -0600

    add components communications

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,EventEmitter,Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -9,9 +9,15 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[]= [
     new Recipe('A Test Recipe', 'just a test', 'http://farm5.static.flickr.com/4083/5057134443_229647c320_z.jpg')
   ];
+
+  @Output() getRecipeDetail = new EventEmitter<Recipe>();
   constructor() { }
 
   ngOnInit() {
+  }
+
+  showRecipeDetail(recipe: Recipe) {
+    this.getRecipeDetail.emit(recipe);
   }
 
 }
